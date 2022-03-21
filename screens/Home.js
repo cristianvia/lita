7dcc780ca9187ec6be9396314ff16682cb99a22b
Cristianvia <cristianviar@gmail.com>
2022-03-21 21:19:08 +0100

    Styling home

--- screens/Home.js
+++ screens/Home.js
@@ -1,22 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { StatusBar } from 'expo-status-bar';
-
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import Colors from "../constants/Colors";
 
 export default () => {
     return (
     <View style={styles.container}>
-        <Text>Hello LiTa</Text>
-        <StatusBar style="auto" />
+        <TouchableOpacity onPress={() => {}} style={styles.itemContainer}>
+            <View><Text style={styles.itemTitle}>Lista 1</Text></View>
+            <View style={{flexDirection:"row"}}>
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="options-outline" size={24} color="white"/>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                    <Ionicons name="trash-outline" size={24} color="white"/>
+                </TouchableOpacity>
+                </View>
+        </TouchableOpacity>
     </View>)
 }
-
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: "#fff",
     },
-  });
+    itemTitle: { fontSize: 24, padding: 5, color: "white" },
+    itemContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        height: 100,
+        flex: 1,
+        borderRadius: 20,
+        marginHorizontal: 20,
+        marginVertical: 10,
+        padding: 15,
+        backgroundColor: Colors.blue,
+    },
+    icon: {
+        padding: 5,
+        fontSize: 24,
+    },
+    centeredView: {
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 50,
+    },
+    modalView: {
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+});
   
