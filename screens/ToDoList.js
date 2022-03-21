1f75e0585ada4681c687d3d7683b2cba042874d0
Cristianvia <cristianviar@gmail.com>
2022-03-21 22:41:58 +0100

    Add todolist

--- screens/ToDoList.js
+++ screens/ToDoList.js
@@ -1,10 +1,47 @@
-import React from "react";
+import React, {useState, useLayoutEffect} from "react";
 import { StyleSheet, Text, View, TouchableOpacity, FlatList } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import ToDoItem from "../components/ToDoItem";
 import Colors from "../constants/Colors";
 
-export default () => {
-    return(<View style={styles.container}></View>);
+const renderAddListIcon = (addItem) => {
+    return (
+        <TouchableOpacity onPress={() => addItem({text: "Hello2", isChecked: false })}>
+            <Text style={styles.icon}>+</Text>
+        </TouchableOpacity>
+    )
+}
+
+
+export default ({navigation}) => {
+    const [toDoItems, setToDoItems] = useState([{text: "hello", isChecked: false}]);
+
+    const addItemToLists = (item) => {
+        toDoItems.push(item);
+        setToDoItems([...toDoItems]);
+    }
+
+    const removeItemFromLists = (index) => {
+        toDoItems.splice(index, 1);
+        setToDoItems([ ...toDoItems ]);
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => renderAddListIcon(addItemToLists)
+        })
+    })
+
+
+    return (<View style={styles.container}>
+        <FlatList
+            data={toDoItems}
+            renderItem={({ item:{text, isChecked}, index }) => {
+                return (<ToDoItem text={text} isChecked={isChecked}/>)
+            }}
+        />
+    </View>
+    );
 }
 
 const styles = StyleSheet.create({
