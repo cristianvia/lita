1db5e71b646b9541c7d3286f42961cc7eabab6cc
Cristianvia <cristianviar@gmail.com>
2022-03-21 21:41:43 +0100

    Add react navigation usability passing props from flatlist to home

--- App.js
+++ App.js
@@ -2,13 +2,27 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./screens/Home";
+import ToDoList from "./screens/ToDoList";
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="LiTa - Lista de Tareas" component={Home} />
+        <Stack.Screen name="LiTapp - Lista de Tareas" component={Home} />
+        <Stack.Screen
+          name="ToDoList"
+          component={ToDoList}
+          options={({ route }) => {
+            return ({
+              title: route.params.title,
+              headerStyle: {
+                backgroundColor: route.params.color
+              },
+              headerTintColor: "white"
+            })
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
